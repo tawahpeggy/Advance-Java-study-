7041e685b4e56bc96104f017b1a00100e2d38897
tawahpeggy <tawahpeggy98@gmail.com>
2022-06-16 00:57:54 +0100

    variables and datatypes in progress

--- hello.java
+++ hello.java
@@ -1,6 +1,15 @@
 public class hello {
     public static void main (String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello peggy!");
+//        Variables
+        int myFirstNumber = (5 + 6) + (2 * 8);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000-myTotal;
+        System.out.print(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.print(myLastOne);
     }
 }
